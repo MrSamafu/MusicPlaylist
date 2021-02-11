6b8fdc2d41dc191197d79508820f3404ca70f133
Samson <51507673+MrSamafu@users.noreply.github.com>
2021-02-11 15:03:58 +0100

    init2

--- MvcMusic/obj/Release/net5.0/MvcMusic.RazorAssemblyInfo.cs
+++ MvcMusic/obj/Release/net5.0/MvcMusic.RazorAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MvcMusic")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("MvcMusic")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MvcMusic")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("MvcMusic.Views")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
